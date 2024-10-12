81275cd8a3227f0a6ffd4bd9aa2977263263e8d2
Ironsyde-tech <ceo_ironsyde@icloud.com>
2024-10-12 22:16:36 +0000

    update

--- public/js/api.js
+++ public/js/api.js
@@ -39,3 +39,48 @@ function listTickets() {
 function getTicketStats() {
     return Promise.resolve({ pending: 84, open: 12, closed: 4 });
 }
+
+function listCourses() {
+    myHeaders.append('authorization', `Bearer ${localStorage.getItem('access_token')}`);
+    return fetch(`${baseUrl}/courses`, {
+        method: "GET",
+        credentials: 'include',
+        headers: myHeaders,
+    })
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+            
+        })
+        .catch((error) => console.error(error));
+}
+
+function viewTimetable() {
+    myHeaders.append('authorization', `Bearer ${localStorage.getItem('access_token')}`);
+    return fetch(`${baseUrl}/timetable`, {
+        method: "GET",
+        credentials: 'include',
+        headers: myHeaders,
+    })
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+            
+        })
+        .catch((error) => console.error(error));
+}
+
+function listLecturers() {
+    myHeaders.append('authorization', `Bearer ${localStorage.getItem('access_token')}`);
+    return fetch(`${baseUrl}/lecturers`, {
+        method: "GET",
+        credentials: 'include',
+        headers: myHeaders,
+    })
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+            
+        })
+        .catch((error) => console.error(error));
+}
